00b501f6561c33db33c350b156a992eb145a9ab1
Nazar <thenazardremach@gmail.com>
2023-12-29 15:08:10 +0100

    version 0.4

--- CulinaryCraft/MenuViewController.swift
+++ CulinaryCraft/MenuViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class MenuViewController: UIViewController {
-
+    
+    @IBOutlet var recipesButtons: [UIButton]!
+    
+    @IBOutlet var recipesNames: [UILabel]!
+    
+    var recipe = [
+        Recipes(name: "Паэлья с креветками",
+                recipet: ""),
+        Recipes(name: "2",
+                recipet: ""),
+        Recipes(name: "3",
+                recipet: ""),
+        Recipes(name: "4",
+                recipet: ""),
+        Recipes(name: "5",
+                recipet: ""),
+        Recipes(name: "6",
+                recipet: ""),
+        Recipes(name: "7",
+                recipet: ""),
+        Recipes(name: "8",
+                recipet: ""),
+        Recipes(name: "9",
+                recipet: ""),
+        Recipes(name: "10",
+                recipet: ""),
+        Recipes(name: "11",
+                recipet: ""),
+]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        completeRecipes()
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else { return }
+        print (sender.tag)
     }
 
-
+    func completeRecipes() {
+        for index in 0..<recipesNames.count {
+            
+            recipesNames[index].text =
+            "\(recipe[index].name)"
+            
+        }
+            
+    }
 }
 
---
+++ CulinaryCraft/RecipesModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecipesModel.swift
+//  CulinaryCraft
+//
+//  Created by Nazar Dremach on 24/12/23.
+//
+
+import Foundation
+
+struct Recipes {
+    let name: String
+    let recipet: String
+}
